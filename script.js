4385d43b00c966a6fa330697e57212b5639dbe0a
lESLY AMAYA <lamayal@miumg.edu.gt>
2025-11-15 08:20:34 -0600

    TrabajarDatos-AMAYA

--- script.js
+++ script.js
@@ -1,31 +1,10 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Calculadora de Total a Pagar</title>
-  <link rel="stylesheet" href="styles.css">
-</head>
-<body>
+unction calcular() {
+  const cliente = document.getElementById('cliente').value.trim();
+  const precio = parseFloat(document.getElementById('precio').value);
+  const cantidad = parseInt(document.getElementById('cantidad').value);
+  const resultado = document.getElementById('resultado');
 
-  <h1>Calculadora de Total a Pagar</h1>
-
-  <div class="container">
-    <label>Nombre del cliente:</label>
-    <input type="text" id="cliente" />
-
-    <label>Precio unitario:</label>
-    <input type="number" id="precio" />
-
-    <label>Cantidad:</label>
-    <input type="number" id="cantidad" />
-
-    <button onclick="calcular()">Calcular total</button>
-
-    <div id="resultado"></div>
-  </div>
-
-  <script src="script.js"></script>
-</body>
-</html>
-
+if (isNaN(precio) || isNaN(cantidad)) {
+    resultado.textContent = "Por favor ingresa precio y cantidad válidos";
+    return;
+  }
